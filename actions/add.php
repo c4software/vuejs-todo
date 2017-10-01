cafe98d9f0a141f9d3e5c99c90f4c7378e72eae3
Valentin Brosseau <c4software@gmail.com>
2017-10-01 18:14:35 +0200

    Update actions to use the id as key

--- actions/add.php
+++ actions/add.php
@@ -1,20 +1,14 @@
 <?php
+session_start();
 
 /**
  * Add a new item in the todolist
  */
 
- session_start();
-
-// Test if Session todos is correctly set
- if(!is_array($_SESSION["todos"])){
-   $_SESSION["todos"] = array();
- }
-
 // Add to history if texte present in the post
  if($_POST["texte"] != ""){
     $todo = array("id" => uniqid(), "texte" => $_POST["texte"], "date" => time(), "termine" => false);
-    array_unshift($_SESSION["todos"], $todo);
+    $_SESSION["todos"][$todo["id"]] = $todo;
 }
 
 // Redirect
