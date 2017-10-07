2075c59e6af972d2eb5199635508a1f49c57c11f
Valentin Brosseau <c4software@gmail.com>
2017-10-07 22:29:46 +0200

    Add function to get data from the server and add the mount call

--- public/main.js
+++ public/main.js
@@ -5,5 +5,37 @@ var app = new Vue({
   },
   created: function () {
     console.log("Démarrage TODO-APP");
+  },
+  beforeMount: function() {
+    this.recupererListe();
+  },
+  methods:{
+    ajout: function () {},
+    recupererListe: function (){
+      // Récupération des todos
+      get_data("api/liste").then(function(todos){
+        // Après la réussite de l’appel, sauvegarde des todos
+        this.todos = todos;
+      }).catch(function(e){
+        console.log(e);
+      })
+    },
+    terminer: function(){},
+    supprimer: function(){}
   }
 });
+
+
+
+function get_data(uri){
+  return new Promise(function(resolve, reject){
+    fetch(uri).then(function(response){
+      // On décode le JSON, et on continue
+      return response.json();
+    }).then(function(response) {
+      resolve(response);
+    }).catch(function(error) {
+      reject(error);
+    });
+  });
+}
