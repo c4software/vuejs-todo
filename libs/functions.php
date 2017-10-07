78d72e9f49940f2e0073472532e80a2c65df90bf
Valentin Brosseau <c4software@gmail.com>
2017-10-07 23:03:12 +0200

    Remove unnecessary lib and function

--- libs/functions.php
+++ libs/functions.php
@@ -8,21 +8,4 @@ function init_todos(){
     $_SESSION["todos"] = array();
   }
 }
-
-function to_bool_01($b){
-  return $b?1:0;
-}
-
-/*
- * Cleanup the HTML to avoid XSS
- */
- function sanitize_html($data) {
-     include_once('./libs/htmlpurifier/HTMLPurifier.auto.php');
-
-     $config = HTMLPurifier_Config::createDefault();
-     $purifier = new HTMLPurifier($config);
-     $data = $purifier->purify($data);
-     return $data;
- }
-
 ?>
