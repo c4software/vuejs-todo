ab0c8b05d90d2a184904d6af6faffa53020af02c
Valentin Brosseau <c4software@gmail.com>
2017-10-07 22:55:04 +0200

    Add « terminer » API

---
+++ api/terminer.php
@@ -0,0 +1,21 @@
+<?php
+
+  session_start();
+  header("content-type: application/json");
+  include("../libs/functions.php");
+  init_todos();
+
+  $id = $_GET["id"];
+  // id in parameters ?
+  if($id != ""){
+    // If todo exist in session.
+    if (array_key_exists($id, $_SESSION["todos"])){
+      // Mark as done.
+      $_SESSION["todos"][$id]["termine"] = true;
+      echo json_encode(array("success" => true));
+    }else{
+      echo json_encode(array("success" => false));
+    }
+  }else{
+    echo json_encode(array("success" => false));    
+  }
